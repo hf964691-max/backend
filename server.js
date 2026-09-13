b86a5403ea1531b65e23bfc11fb28c3017dcffb4
Huzaifa <hf964691@gmail.com>
2026-09-13 16:01:58 +0530

    github data added

--- server.js
+++ server.js
@@ -1,12 +1,54 @@
-require('dotenv').config();
+require("dotenv").config();
 const express = require("express");
 
 const app = express();
 
 const port = process.env.PORT || 4000;
 
+const githubData = {
+  login: "hf964691-max",
+  id: 221438751,
+  node_id: "U_kgDODTLjHw",
+  avatar_url: "https://avatars.githubusercontent.com/u/221438751?v=4",
+  gravatar_id: "",
+  url: "https://api.github.com/users/hf964691-max",
+  html_url: "https://github.com/hf964691-max",
+  followers_url: "https://api.github.com/users/hf964691-max/followers",
+  following_url:
+    "https://api.github.com/users/hf964691-max/following{/other_user}",
+  gists_url: "https://api.github.com/users/hf964691-max/gists{/gist_id}",
+  starred_url:
+    "https://api.github.com/users/hf964691-max/starred{/owner}{/repo}",
+  subscriptions_url: "https://api.github.com/users/hf964691-max/subscriptions",
+  organizations_url: "https://api.github.com/users/hf964691-max/orgs",
+  repos_url: "https://api.github.com/users/hf964691-max/repos",
+  events_url: "https://api.github.com/users/hf964691-max/events{/privacy}",
+  received_events_url:
+    "https://api.github.com/users/hf964691-max/received_events",
+  type: "User",
+  user_view_type: "public",
+  site_admin: false,
+  name: "Huzaifa",
+  company: null,
+  blog: "",
+  location: null,
+  email: null,
+  hireable: true,
+  bio: null,
+  twitter_username: null,
+  public_repos: 5,
+  public_gists: 0,
+  followers: 3,
+  following: 6,
+  created_at: "2025-07-18T00:03:52Z",
+  updated_at: "2026-09-13T09:30:18Z",
+};
+
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send(`
+    <h1>Hello World!</h1>
+    <h2>Welcome to my first backend Website</h2>
+    `);
 });
 
 app.get("/twitter", (req, res) => {
@@ -20,6 +62,10 @@ app.get("/login", (req, res) => {
 app.get("/youtube", (req, res) => {
   res.send(`<h2>Chai aur Code</h2>`);
 });
+
+app.get('/github', (req, res) => {
+    res.json(githubData)
+})
 
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
